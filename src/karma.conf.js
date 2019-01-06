146c501a5d185ee523362ed2fecb16d968523cae
Shailendra Tiwari <sheelu.imsg@gmail.com>
2019-01-06 17:58:56 +0530

    setup junit test for vsts

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -8,6 +8,8 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'), // add the phantom launcher
+      require('karma-junit-reporter'), // add the phantom launcher
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
